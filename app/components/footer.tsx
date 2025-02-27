f1d3f9ef508ef8b0d3cfc927af311688c1c2594d
developereciid <developereciid@gmail.com>
2025-02-27 12:04:43 +0700

    update footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,15 +6,15 @@ export default function Footer() {
       <div className="container mx-auto px-4">
         <div className="flex justify-center">
           <div className="flex gap-6">
-            <a href="mailto:kurniaputri13@gmail.com" className="text-gray-400 hover:text-white">
+            <a href="mailto:krnputri13@gmail.com" className="text-gray-400 hover:text-white">
               Email
             </a>
             <a href="https://www.linkedin.com/in/krnputri" className="text-gray-400 hover:text-white">
               LinkedIn
             </a>
-            <a href="https://www.instagram.com/krniaputri" className="text-gray-400 hover:text-white">
+            {/* <a href="https://www.instagram.com/krniaputri" className="text-gray-400 hover:text-white">
               Instagram
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
